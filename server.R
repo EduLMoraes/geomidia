a9ae94ef30734a6e6b3256a6a6dc320a6951a812
eduardomoreaes <17eduardo05@gmail.com>
2022-09-03 13:01:10 -0300

    modified:   .RData 	modified:   .Rhistory 	modified:   .gitignore 	deleted:    Index.R 	new file:   app.R 	new file:   geodata/mundo/rankingArea.csv 	new file:   geopol.html 	new file:   server.R 	new file:   ui.R 	new file:   www/style.css

---
+++ server.R
@@ -0,0 +1,206 @@
+####    Mapas     ####
+mundo      <- readOGR('C:/Users/Elvins Moraes/Desktop/edu/Projeto - Geopolitica/geodata/mundo/mundoGeopolitico.shp')
+continente <- readOGR('C:/Users/Elvins Moraes/Desktop/edu/Projeto - Geopolitica/geodata/continentes/Continents.shp')
+
+## MAPAS GENÉRICOS ##
+mapaPaises <- leaflet(mundo) %>% setView(lat=0, lng=0, zoom = 2) %>% addTiles(options = tileOptions(minZoom = 2, maxZoom = 10, maxNativeZoom = 5), urlTemplate = "http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga")
+mapaContin <- leaflet(continente) %>% setView(lat=0, lng=0, zoom = 2) %>% addTiles(options = tileOptions(minZoom = 2, maxZoom = 10, maxNativeZoom = 5), urlTemplate = "http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga") %>% addPolygons(color = "black", label = continente@data$PLACENAME, weight = 1, opacity = 0.5)
+
+
+#       MAPA GM1    #
+mapaGM1    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corGM1, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA GM2    #
+mapaGM2    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corGM2, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA G7     #
+mapaG7    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corG7, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA G20
+mapaG20    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corG20, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA ONU    #
+mapaOnu    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corOnu, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA OTAN   #
+mapaOtan49    <- mapaPaises %>%
+  addPolygons(color = mundo@data$cOTAN_4, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+mapaOtan22    <- mapaPaises %>%
+  addPolygons(color = mundo@data$cOTAN_2, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA PACTO  #
+mapaPac    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corPact, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA IMPERIO#
+mapaImp    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corIMP, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA SISTEMA#
+mapaSis   <- mapaPaises %>%
+  addPolygons(color = mundo@data$corSis, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA LADO   #
+mapaLado    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corLado, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+#       MAPA ALIANÇA#
+mapaAli    <- mapaPaises %>%
+  addPolygons(color = mundo@data$corTRI, label = mundo@data$CNTRY_N, weight = 1, opacity = 1)
+
+
+####    Graficos  ####
+tamanho <- read.csv2("C:/Users/Elvins Moraes/Desktop/edu/Projeto - Geopolitica/geodata/mundo/rankingArea.csv")
+ranking <- tamanho[,-c(1:2)]
+ranking <- as.data.frame(ranking)
+row.names(ranking) <- tamanho$Paises
+
+
+####    Server    ####
+server <- function(input, output,  session){
+  
+  
+  output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking dos Países por Território")
+  output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaContin)
+  output$texto = renderUI(includeHTML("geopol.html"))
+  
+  renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+  renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+  renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+    
+  
+  observeEvent(input$inicio,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking dos Países por Território")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaContin)
+    output$texto = renderUI(includeHTML("geopol.html"))
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  
+  observeEvent(input$gm1,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaGM1)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$gm2,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaGM2)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+
+  
+  observeEvent(input$pgm,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaGM1)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$sgm,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaGM2)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$otan,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaOtan22)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$onu,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaOnu)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$pct,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaPac)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$imp,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaImp)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$gst,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaG7)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$gvt,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaG20)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$sis,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaSis)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$lsg,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaLado)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+  
+  observeEvent(input$ali,{
+    output$GraficoPie = renderText("Divisão Global"); output$GraficoBar = renderText("Ranking Países")
+    output$GraficoLin = renderText("Linha Temporal"); output$mapa = renderLeaflet(mapaAli)
+    
+    renderPieChart(div_id = "graph_pie", data = mundo@data$SIS_POL, theme = 'roma', show.tools=F, show.legend = F,  radius = "80%")
+    renderLineChart(div_id = "graph_line", data = ranking, theme = 'roma', show.tools=F, show.legend = F)
+    renderBarChart(div_id = "graph_bar", data = ranking, theme = 'roma', show.tools=T, show.legend = F, direction="vertical", grid_left = '3%', font.size.legend=15)
+  })
+}
---
+++ ui.R
@@ -0,0 +1,106 @@
+####    Dashboard ####
+temas <- c('tema1', 'tema2', 'tema3', 'tema4')
+skin <- 'red'
+gm <- c("Guerras & Conflitos", "1ª Guerra Mundial" = 1, "2ª Guerra Mundial" = 2, "Guerra Fria" = 3)
+
+ui <- dashboardPage(skin = skin,
+                    title = "GEOPOLÍTICA",
+                    
+     # HEADER     -------------------------------------------------------
+                    dashboardHeader(
+                      title = "GEOPOLÍTICA",
+                      titleWidth = 300
+                    ),
+     # SIDEBAR    -------------------------------------------------------               
+                    dashboardSidebar(
+                      width = 300,
+                      tags$div(class = "alinhamento"),
+                      
+                      sidebarMenu(
+                        div(id = "sidebar_button",
+                            bsButton(inputId = "inicio", label = "INICIO", icon = icon("play-circle"), style = "danger")
+                        )
+                      ),
+                      br(),
+                      # Guerra             -----
+                      menuItem("GUERRA", tabName = "antimicrobials", icon = icon("fire"),
+                        menuItem( "1ª GUERRA MUNDIAL \n (1914-1918)",
+                                actionButton(inputId = "gm1", label = "Alianças Militares", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;"),
+                                actionButton(inputId = "imp", label = "Impérios Centrais", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;"),
+                                actionButton(inputId = "ali", label = "Alianças Militares", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;")
+                        ),
+                        menuItem("2ª GUERRA MUNDIAL (1939-1945)",
+                                actionButton(inputId = "gm2", label = "Alianças Militares", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;"),
+                                actionButton(inputId = "lsg", label = "Lados da Segunda Guerra", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;")
+                        ),
+                        menuItem("GUERRA FRIA (1945-1991)")
+                      ),
+                      br(),
+                      # Organização        -----
+                      menuItem("ORGANIZÇOES", tabName = "organizacoes", icon = icon("cog", lib = "glyphicon"),
+                               actionButton(inputId = "onu", label = "ONU", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;"),
+                               actionButton(inputId = "gst", label = "G7", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;"),
+                               actionButton(inputId = "gvt", label = "G20", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;")
+                      ),
+                      br(),
+                      # Alianças Militares ----
+                      menuItem("ALIANÇAS MILITARES", tabName = "aliancas", icon = icon("list-alt"),
+                               actionButton(inputId = "otan", label = "OTAN", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;"),
+                               actionButton(inputId = "pct", label = "Pacto de Varsóvia", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;")
+                               
+                      ),
+                      br(),
+                      # Sistemas Políticos ----
+                      menuItem("SISTEMAS POLÍTICOS", tabName = "aliancas", icon = icon("money"),
+                               actionButton(inputId = "sis", label = "Sistemas Políticos", style="width:88%; background-color:black; color:white; margin-left:15px; margin-top:5px;")
+                      ),
+                      br()
+                    ),
+     # BODY       -------------------------------------------------------
+                    dashboardBody(
+                      tags$head(
+                        tags$link(
+                          rel = "stylesheet", 
+                          type = "text/css", 
+                          href = "style.css")
+                      ),
+                      
+                      useShinyjs(),
+                      
+                      loadEChartsLibrary(),
+                      loadEChartsTheme('vintage'),
+                      loadEChartsTheme('roma'),
+                      loadEChartsTheme('macarons'),
+                      loadEChartsTheme('jazz'),
+                      
+                      fluidRow(
+                        leafletOutput(outputId = "mapa")
+                      ),
+                      
+                      
+                      fluidRow(
+                        column(3,
+                               textOutput(outputId = "GraficoPie"),
+                               tags$div(id="graph_pie", style="width:100%;height:200px;"),  # Specify the div for the chart.
+                               deliverChart(div_id = "graph_pie"),
+                        ),
+                        column(5,
+                               # Deliver the plotting
+                               
+                               textOutput(outputId = "GraficoBar"),
+                               tags$div(id="graph_bar", style="width:100%;height:200px;"),
+                               deliverChart(div_id = "graph_bar")
+                        ),
+                        column(4,
+                               textOutput(outputId = "GraficoLin"),
+                               tags$div(id="graph_line", style="width:100%;height:200px;"),
+                               deliverChart(div_id = "graph_line")
+                        )
+                      ),
+                      
+                      
+                      fluidRow(
+                        htmlOutput(outputId = "texto")
+                      )
+                    )         
+)
